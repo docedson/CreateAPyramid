f3480c204878ce137bfa7e6cb114e7ab7e59273a
Andrew Edson <docedson@gmail.com>
2019-04-10 16:29:03 -0500

    Add project files.

---
+++ CreateAPyramid/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                for (int space = 0; space < 5 - row - 1; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int star = 0; star < row * 2 + 1; star++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
